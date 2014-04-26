acd800d870ed60603357a2a6634f0832bea9a3b3
lingdongrushui <lingdongrushui@gmail>
2014-04-26 14:14:24 +0800

    update jquery pagination plugin demo and lib

---
+++ demo/js/test.js
@@ -0,0 +1,8 @@
+var test = function(){
+	var array = new Array();
+	array.push();
+}
+
+var jsBattle = function() {
+	console.info("Prepare a JS battle now");
+}
---
+++ demo/lib/js/controllers.js
@@ -0,0 +1,30 @@
+function PhoneListCtrl($scope){
+	$scope.phones = [
+		{
+			"name":"Nexus S",
+			"snippet":"Fast just got faster with Nexus S."
+		},
+		{
+			"name":"Motolola XOOM™ with Wi-Fi",
+			"snippet":"The Next ,Next Generation tablet.",
+		},
+		{
+			"name":"Motolola XOOM™",
+			"snippet":"The Next ,Next Generation tablet.",
+		},
+		{
+			"name":"Nexus S2",
+			"snippet":"Nexus S2 Fast just got faster with Nexus S."
+		},
+		{
+			"name":"Motolola2 XOOM™ with Wi-Fi",
+			"snippet":" Motolola2The Next ,Next Generation tablet.",
+		},
+		{
+			"name":"Motolola2 XOOM™",
+			"snippet":"Motolola2 The Next ,Next Generation tablet.",
+		}
+	];
+
+	$scope.hello = "hello,world !"
+}
